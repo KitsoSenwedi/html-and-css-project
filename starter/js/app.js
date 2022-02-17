14251565a1fb261e7552ae02eafeb077d660e3c2
kitso <senwedikitso@gmail.com>
2022-02-17 03:29:07 +0200

    footer

--- starter/js/app.js
+++ starter/js/app.js
@@ -2,7 +2,7 @@ const getElement = (selector) =>{
     const element = document.querySelector(selector);
     if(element) return element
     throw Error(
-        'Please double check your class names, there is no ${selector} class'
+        `Please double check your class names, there is no ${selector} class`
         )
 }
 
